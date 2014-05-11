7b6300e753d989f8780d91e701c5d7ef7ca5b59a
Douglas Pike <doughp1@firestar.(none)>
2014-05-11 19:57:44 -0400

    checkpoint_update

--- qrc_bitcoin.cpp
+++ qrc_bitcoin.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Resource object code
 **
-** Created: Sun May 11 08:10:28 2014
+** Created: Sun May 11 18:49:16 2014
 **      by: The Resource Compiler for Qt version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost!
